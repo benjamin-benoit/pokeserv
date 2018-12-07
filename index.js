416ae3a0cea044070b967e9d2c3de45468f4e8e9
BENOIT <benjaminbenoitfr@gmail.com>
2018-12-07 17:03:00 +0100

    All auto indented

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-var bodyParser = require('body-parser');
-var express = require('express');
+var bodyParser = require("body-parser");
+var express = require("express");
 var app = express();
 var fs = require("fs");
 var obj = require("./pokedex.json");
-const fileContents = fs.readFileSync('./pokedex.json', 'utf8')
+const fileContents = fs.readFileSync("./pokedex.json", "utf8");
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 app.get("/liste", (req, res, next) => {
   try {
-    const data = JSON.parse(fileContents)
+    const data = JSON.parse(fileContents);
     res.json(data);
-  } catch(err) {
-    console.error(err)
-    res.json({"":""});
+  } catch (err) {
+    console.error(err);
+    res.json({ "": "" });
   }
 });
 
 app.get("/pokemon/:id", function(req, res) {
   const idSelected = req.params.id;
-  const data = JSON.parse(fileContents)
+  const data = JSON.parse(fileContents);
 
   let found = data.find(function(element) {
-    return req.params.id === element.ndex
+    return req.params.id === element.ndex;
   });
   console.log(found);
   res.send(found);
